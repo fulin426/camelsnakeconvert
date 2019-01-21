d33f708f358c235d6651ae05f5f3a2a7e464156f
fulin426 <liufam@ms-MacBook-Pro.local>
2019-01-20 22:16:26 -0800

    adding build

---
+++ build/precache-manifest.d66a5fd0ea0dc9cb8469ff688b8bc757.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a7353826c7c050d24c46",
+    "url": "/static/js/main.a7353826.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "a7353826c7c050d24c46",
+    "url": "/static/css/main.36389a0f.chunk.css"
+  },
+  {
+    "revision": "0914c6f769781c00f3e7f094a3b5a940",
+    "url": "/index.html"
+  }
+];
