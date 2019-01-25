6497d3742ff3b68f67972d14d9f7239961646c8a
fulin426 <liufam@ms-MacBook-Pro.local>
2019-01-24 21:52:01 -0800

    commit new build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d66a5fd0ea0dc9cb8469ff688b8bc757.js"
+  "/precache-manifest.23fd607c1fdd0af5807536eb0290b1b7.js"
 );
 
 workbox.clientsClaim();
